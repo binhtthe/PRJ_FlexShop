e08b7247785ad16c4d767feb1616967f89907f1e
binhtthe <binhlop789@gmail.com>
2025-11-18 16:41:11 +0700

    create dbset

--- PRJ_MKS_BTT/Data/ApplicationDbContext.cs
+++ PRJ_MKS_BTT/Data/ApplicationDbContext.cs
@@ -1,8 +1,49 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using PRJ_MKS_BTT.Model;
 namespace PRJ_MKS_BTT.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart>  Carts { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<CartItem> CartItems { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Shipment> Shipments { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Sku> Skus { get; set; }
+        public DbSet<UserAddress> UserAddresses { get; set; }
+        public DbSet<Wishlist> Wishlists { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<OrderVoucher> OrderVouchers { get; set; }
+
+        public DbSet<Voucher> Vouchers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<Order>()
+                .HasIndex(o => o.OrderNumber)
+                .IsUnique();
+                
+        }
+
     }
 }
